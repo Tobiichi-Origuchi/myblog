6c041e7656d4c7ad5cf343db8197a5820ff594da
tobiichioriguchi <tobiichioriguchi@gmail.com>
2026-04-17 20:33:15 +0800

    edit csp to allow giscus load

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -11,10 +11,10 @@ export const onRequest: PagesFunction = async (context) => {
     `default-src 'self'; ` +
     `script-src 'self' 'nonce-${nonce}' https://giscus.app https://static.cloudflareinsights.com; ` +
     `connect-src 'self' https://giscus.app https://cloudflareinsights.com; ` +
-    `style-src 'self' 'unsafe-inline'; ` +
+    `style-src 'self' 'unsafe-inline' https://giscus.app; ` +
     `img-src 'self' data: https:; ` +
     `media-src 'self' https:; ` +
-    `frame-src 'self' https://challenges.cloudflare.com; ` +
+    `frame-src 'self' https://giscus.app https://challenges.cloudflare.com; ` +
     `font-src 'self'; ` +
     `object-src 'none'; ` +
     `base-uri 'none'; ` +
