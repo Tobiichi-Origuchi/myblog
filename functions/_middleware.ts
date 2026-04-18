225b32cb2dc3849e4c815abc681701e253c07276
Origuchi <tobiichioriguchi@gmail.com>
2026-04-18 16:12:27 +0800

    update

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -22,7 +22,7 @@ export const onRequest: PagesFunction = async (context: { next: () => any; }) =>
     `default-src 'self'; ` +
     `script-src 'self' 'nonce-${nonce}' https://giscus.app https://static.cloudflareinsights.com; ` +
     `connect-src 'self' https://giscus.app https://cloudflareinsights.com; ` +
-    `style-src 'self' https://giscus.app; ` +
+    `style-src 'self' 'nonce-${nonce}' https://giscus.app; ` +
     `img-src 'self' data: https:; ` +
     `media-src 'self' https:; ` +
     `frame-src 'self' https://giscus.app https://challenges.cloudflare.com; ` +
